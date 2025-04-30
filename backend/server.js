ee096d5c8e17d26bfe3c3544cf273716a3265641
Pearly Li <pearlyli@protonmail.com>
2025-04-30 00:41:25 -0700

    Create connection scaffold

--- backend/server.js
+++ backend/server.js
@@ -3,46 +3,19 @@ const app = express();
 
 const mongoose = require("mongoose");
 
-const usersSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        lowercase: true,
-    },
-    password: {
-        type: String,
-        minimumLength: 10,
-        required: true,
-    },
-});
-module.exports = User;
-
-const User = mongoose.model("User", usersSchema);
-
-const plantSchema = new mongoose.Schema({
-    type: String,
-    name: String,
-    items: [String],
-});
-
-const Plant = mongoose.model("Plant", plantSchema);
-
-module.exports = Plant;
-
 main().catch((err) => console.log(err));
 /* Cors is used for preventing web pages from making requests to a different domain than the one that served the web page unless specified
  */
 const cors = require("cors");
 app.use(cors());
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// });
+async function main() {
+    await mongoose
+        .connect
+        // Local host
+        // "mongodb://127.0.0.1:27017/test"
 
-// app.get("/", (req, res) => {
-//     res.send("Hello World!");
-// });
+        // Mongo DB Atlas
+        // mongoose.connect("mongodb+srv://mongo:mongo@cluster0.eyhty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+        ();
+}
