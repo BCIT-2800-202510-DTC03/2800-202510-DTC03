6129d6ccb0b6029a729c8eb66b8f6340e8a12825
Zoey Jiang <zoeyj8170@gmail.com>
2025-05-02 12:30:56 -0700

    Extract user login logic into user.js and implement hashing

--- backend/server.js
+++ backend/server.js
@@ -27,61 +27,14 @@ app.use(express.urlencoded({ extended: true }));
 // }
 
 
+// https://expressjs.com/en/guide/routing.html
+const userRouter = require('./user')
+app.use('/user', userRouter)
 
-const usersArr = [
-  { email: 'admin1@test.com', password: 'admin1' },
-  { email: 'admin2@test.com', password: 'admin2' },
-  { email: 'user1@test.com', password: 'password1' },
-  { email: 'user2@test.com', password: 'password2' },
-  { email: 'user3@test.com', password: 'password3' }
-];
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.post('/user/login', (req, res) => {
-  const { email, password } = req.body;
-  const user = usersArr.find(user => user.email === email && user.password === password);
-
-  if (user) {
-    res.send({
-      message: `Welcome ${email}`,
-    });
-  } else {
-    res.status(401).json({error_message: "Invalid credentials"});
-  }
- })
-
-app.post('/user/register', (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({
-      error_message: "email and password are required"
-    });
-  }
-
-  const userExists = usersArr.some(user => user.email === email);
-  if (userExists) {
-    return res.status(409).json({
-      error_message: "email already exists"
-    });
-  }
-
-  const newUser = {
-    email,
-    password
-  };
-  usersArr.push(newUser);
-
-  res.status(200).json({
-    message: "Registration successful",
-    email: newUser.email
-  });
-});
-
-
- app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ backend/user.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const router = express.Router()
+
+// hashing function from nodejs
+// https://nodejs.org/api/crypto.html#class-hash
+const {
+    createHash,
+} = require('node:crypto');
+
+function hashing(input) {
+    return createHash('sha256')
+        .update(input)
+        .digest('hex');
+}
+  
+// mockdata, will be update after database connection done
+let usersArr = [
+    { email: 'admin1@test.com', password: 'admin1' },
+    { email: 'admin2@test.com', password: 'admin2' },
+    { email: 'user1@test.com', password: 'password1' },
+    { email: 'user2@test.com', password: 'password2' },
+    { email: 'user3@test.com', password: 'password3' }
+];
+
+usersArr = usersArr.map(user => ({email: user.email, password: hashing(user.password)}))
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = usersArr.find(user => user.email === email && user.password === hashing(password));
+
+    if (user) {
+        res.send({
+        message: `Welcome ${email}`,
+        });
+    } else {
+        res.status(401).json({error_message: "Invalid credentials"});
+    }
+})
+
+router.post('/register', (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+        error_message: "Email and password are required"
+        });
+    }
+
+    const userExists = usersArr.some(user => user.email === email);
+    if (userExists) {
+        return res.status(409).json({
+        error_message: "Email already exists"
+        });
+    }
+
+    const newUser = {
+        email: email,
+        password: hashing(password)
+    };
+    usersArr.push(newUser);
+
+    res.status(200).json({
+        message: "Registration successful",
+        email: newUser.email
+    });
+})
+
+module.exports = router
