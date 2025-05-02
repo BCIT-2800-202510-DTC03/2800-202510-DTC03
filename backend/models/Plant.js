7217ea4a04f0ec661840717eb26900c1122580e4
Pearly Li <pli103@my.bcit.ca>
2025-05-02 13:28:12 -0700

    Add quantity to plantSchema

--- backend/models/Plant.js
+++ backend/models/Plant.js
@@ -2,7 +2,20 @@ const mongoose = require("mongoose");
 
 const plantSchema = new mongoose.Schema({
     name: String,
-    rewards: [{ type: mongoose.Schema.Types.ObjectId, ref: "Reward" }],
+    rewards: [
+        {
+            reward: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Reward",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                default: 0,
+                min: 0,
+            },
+        },
+    ],
 });
 
 const Plant = mongoose.model("Plant", plantSchema);
