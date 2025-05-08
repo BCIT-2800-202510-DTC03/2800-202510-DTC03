85549ea57a62cc1f2539ded2808e5f197fcddceb
Pearly Li <pli103@my.bcit.ca>
2025-05-08 12:27:25 -0700

    Add dependencies and some routes to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const session = require("express-session");
+const path = require("path"); /* Needed for working with directories and file paths */
 require("dotenv").config();
 
 const connectToMongo = require("./db"); /* Reference db.js */
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+/* View engine EJS */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 /* Middleware to parse JSON and form data */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/* For serving up static files if we need to host our own images depending on bandwith restrictions for mongodb/cloundinary. This means anything that needs to be accessed publicy will be in the public folder, not yet made. */
+app.use(express.static("public"));
 
 /* Session setup */
 app.use(
@@ -21,10 +29,19 @@ app.use(
 
 connectToMongo();
 
-app.get("/", (req, res) => {
-    res.send("Server connected to MongoDB");
-});
+/* Routes */
+// app.use("/", authRoutes); /* These aren't made yet, so just placeholders */
+// app.use("/tasks", taskRoutes);
+// app.use("/rewards", rewardRoutes);
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+/* Testing purposes */
+// app.get("/", (req, res) => {
+//     res.send("Server connected to MongoDB");
+// });
+
+/* Login */
+app.get("/", (req, res) => res.redirect("/login"));
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
