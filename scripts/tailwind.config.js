46db09630f7ce1d6c5bafd9fc26bcf7986dab9f3
Pearly Li <pli103@my.bcit.ca>
2025-05-08 21:23:30 -0700

    Replace old tailwind.config.js

--- scripts/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    content: ["./views/*.ejs", "./scripts/*.js"], // Includes Tailwind styling for any ejs or js file in these folders
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
-};
