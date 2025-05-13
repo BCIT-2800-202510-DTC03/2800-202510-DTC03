38b046103583b4b90ab02c06234988d0d60e4af6
Kanon Nishiyama <knishiyama@my.bcit.ca>
2025-05-13 14:18:03 -0700

    Create basic asset insertion

---
+++ frontend/scripts/garden.js
@@ -0,0 +1,81 @@
+weather = "clear"
+
+function insertBackground() {
+    const background = document.getElementById("garden-background");
+
+    background.style.backgroundImage = "url('../assets/garden/background-" + weather + ".png')";
+    
+    if (background.style.backgroundImage && weather) {
+        background.style.backgroundColor = "transparent";
+    }
+
+}
+
+function insertFence(type) {
+    const fence = document.getElementById("garden-fence");
+
+    fence.style.backgroundImage = "url('../assets/garden/fence-" + type + ".png')";
+    
+    if (fence.style.backgroundImage || !type) {
+        fence.style.backgroundColor = "transparent";
+    }
+
+}
+
+function insertBuilding(type) {
+    const building = document.getElementById("garden-building");
+
+    building.style.backgroundImage = "url('../assets/garden/building-" + type + ".png')";
+    
+    if (building.style.backgroundImage && type) {
+        building.style.backgroundColor = "transparent";
+    }
+
+}
+
+function insertShelf(type) {
+    const shelf = document.getElementById("garden-shelf");
+
+    shelf.style.backgroundImage = "url('../assets/garden/shelf-" + type + ".png')";
+    
+    if (shelf.style.backgroundImage && type) {
+        shelf.style.backgroundColor = "transparent";
+    }
+
+}
+
+function insertObject(type, select) {
+    const elementID = `garden-${select}Object`;
+    const object = document.getElementById(elementID);
+
+    object.style.backgroundImage = "url('../assets/garden/object-" + type + ".png')";
+    
+    if (object.style.backgroundImage && type) {
+        object.style.backgroundColor = "transparent";
+    }
+
+}
+
+function insertPlant(type, select) {
+    const elementID = `#plant-${select}.garden-plant`;
+    const plant = document.querySelector(elementID);
+
+    plant.style.backgroundImage = "url('../assets/garden/plant-" + type + ".png')";
+    
+    if (plant.style.backgroundImage && type) {
+        plant.style.backgroundColor = "transparent";
+    }
+
+}
+
+async function setup() {
+    insertBackground();
+    insertFence("black");
+    insertBuilding("tent-yellow");
+    insertShelf("blue");
+    insertObject("stool-pink", "right");
+    insertPlant("tulip-yellow", "6")
+    insertPlant("tulip-pink", "2")
+}
+
+setup();
