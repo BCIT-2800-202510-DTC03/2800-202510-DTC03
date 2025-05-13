f4a1430bf5943d57056be30e766390f6658cab28
Pearly Li <pli103@my.bcit.ca>
2025-05-12 23:53:05 -0700

    Add settingsForm rendering for update account settings

--- frontend/scripts/settings.js
+++ frontend/scripts/settings.js
@@ -20,3 +20,43 @@ async function loadUserSettings() {
     }
 }
 loadUserSettings();
+
+// Add update rendering
+const settingsForm = document.getElementById("settingsForm");
+settingsForm.addEventListener("submit", async (event) => {
+    event.preventDefault(); // Prevent post if their password(s) is/are incorrect
+
+    const oldPassword = document.getElementById("oldPassword").value;
+    const newPassword = document.getElementById("newPassword").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const email = document.getElementById("emailInput").value;
+
+    try {
+        const res = await fetch("/settings/update", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email,
+                oldPassword,
+                newPassword,
+                confirmPassword,
+            }),
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            alert(data.error); // 👈 show alert
+            return;
+        }
+
+        alert("Settings updated.");
+        window.location.reload();
+    } catch (err) {
+        alert("Unexpected error occurred.");
+        console.error(err);
+    }
+});
