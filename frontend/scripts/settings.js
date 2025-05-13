6c6c0e6a504eb4c16b61f9bcb47a79d92395f278
Pearly Li <pli103@my.bcit.ca>
2025-05-12 23:38:14 -0700

    Change fetch request for backend testing

--- frontend/scripts/settings.js
+++ frontend/scripts/settings.js
@@ -1,6 +1,9 @@
 async function loadUserSettings() {
     try {
-        const load = await fetch("/pages/settings", { credentials: "include" });
+        // const load = await fetch("/pages/settings", { credentials: "include" });
+        const load = await fetch("http://localhost:3000/settings", {
+            credentials: "include",
+        }); // Just for testing purposes locally
         const userData = await load.json();
 
         if (userData.error) {
@@ -8,7 +11,7 @@ async function loadUserSettings() {
             return;
         }
         const userIdInput = document.getElementById("userId");
-        userIdInput.value = userData.userId;
+        userIdInput.value = userData._id;
         userIdInput.value.readOnly = true;
         document.getElementById("emailInput").value = userData.email;
     } catch (error) {
