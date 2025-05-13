57b1b0e2c75df07c6338fbf8193117ef95cebfab
Zoey Jiang <zoeyj8170@gmail.com>
2025-05-12 22:48:12 -0700

    Fix the cookie setting to git chrome setting

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 3000;
 /* Middleware to parse JSON and form data */
 app.use(
   cors({
-    origin: true,
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "https://two800bloomgreener.onrender.com",
+    ],
     credentials: true,
   })
 );
@@ -29,9 +33,16 @@ app.use(
     saveUninitialized: false,
 
     cookie: {
+      httpOnly: true,
       secure: false,
-      maxAge: 1000 * 60 * 60,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
     },
+    // for deploy in future
+    //     cookie: {
+    //   httpOnly: true,
+    //   secure: true,
+    // }
   })
 );
 
--- backend/user.js
+++ backend/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+var session = require("express-session");
 
 // import user model
 const User = require("./models/User");
