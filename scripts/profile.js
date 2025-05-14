ca713c8eee35e73a610456f39e9808e22b1cd92b
Genevieve <gglaim@my.bcit.ca>
2025-05-14 11:53:19 -0700

    set up event listeners

--- scripts/profile.js
+++ scripts/profile.js
@@ -2,7 +2,13 @@ const editPencil = document.getElementById("edit-pencil");
 const pfpOptions = document.getElementById("pfp-choices-wrap");
 const buttons = document.querySelectorAll('.pfp-image-radio');
 const pfp = document.getElementById("profile-picture");
+const aboutMe = document.getElementById("about-me-txt");
+const goalSelect = document.getElementById("goals");
 
+
+var aboutContent;
+var pfpPreference;
+var userGoal;
 
 function profilePictureSetup() {
     editPencil.addEventListener("click", (event) => {
@@ -24,6 +30,25 @@ function updateUserPreference() {
     //send stuff to DB
 }
 
+
+function loadUserPreferences() {
+    //get stuff from DB
+
+    if(aboutContent){
+        aboutMe.value = aboutContent;
+    }
+    if(pfpPreference){
+        pfp.src = pfpPreference;
+    } else {
+        //update this with the default image we want to use
+        pfp.src = "https://dummyimage.com/100/606c38/dadbe6";
+    }
+
+    if (userGoal){
+        goalSelect.value = userGoal;
+    }
+}
+
 function radioButtonSetup() {
     buttons.forEach(btn => {
         btn.addEventListener("change", () => {
@@ -36,11 +61,34 @@ function radioButtonSetup() {
     })
 }
 
+function aboutMetSetup() {
+    aboutMe.addEventListener("input", () => {
+        //update as the user types
+        aboutContent = aboutMe.value;
+        // console.log(aboutContent);
+    })
+    aboutMe.addEventListener("change", () => {
+        //when user finished typing/clicks off of input: update user info
+        updateUserPreference();
+        // console.log(aboutContent);
+    })
+}
+
+function goalSetup() {
+    goalSelect.addEventListener("change", () => {
+        userGoal = goalSelect.value;
+        // console.log(userGoal);
+    })
+}
+
 
 
 function main() {
+    loadUserPreferences();
     profilePictureSetup();
     radioButtonSetup();
+    aboutMetSetup();
+    goalSetup();
 }
 
 main();
