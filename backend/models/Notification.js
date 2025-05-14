1523e45f52692f9819d25916628be5af8c7b6736
Pearly Li <pli103@my.bcit.ca>
2025-05-14 14:16:39 -0700

    Simplify notificationSchema

--- backend/models/Notification.js
+++ backend/models/Notification.js
@@ -2,24 +2,26 @@ const mongoose = require("mongoose");
 /* For more information, see https://mongoosejs.com/docs/schematypes.html#objectids */
 
 const notificationSchema = new mongoose.Schema({
-    Task: {
+    task: {
         type: Schema.Types.ObjectId,
         reference: "Task",
         required: true,
     },
-    enabled: {
-        type: Boolean,
-        default: true,
-    },
-    channel: {
-        email: { type: Boolean, default: false },
-        push: { type: Boolean, default: true },
-    },
-    triggerDay: Date,
-    notificationTimeMinutesBefore: {
-        type: Number,
-        default: 60,
-    },
+
+    // Simplify notification structure until I can get it working
+    // enabled: {
+    //     type: Boolean,
+    //     default: true,
+    // },
+    // channel: {
+    //     email: { type: Boolean, default: false },
+    //     push: { type: Boolean, default: true },
+    // },
+    // triggerDay: Date,
+    // notificationTimeMinutesBefore: {
+    //     type: Number,
+    //     default: 60,
+    // },
     isRecurring: { type: Boolean, default: false },
     intervalInDays: { type: Number },
     repeatUntil: { type: Date },
