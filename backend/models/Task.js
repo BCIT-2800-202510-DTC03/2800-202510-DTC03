d50b2bb993b85ae1511cf3175a2f227574a49c97
Pearly Li <pli103@my.bcit.ca>
2025-05-14 14:17:20 -0700

    Add notification to taskSchema

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -4,6 +4,8 @@ const taskSchema = new mongoose.Schema({
     name: String,
     description: String,
     category: String,
+    notifyAt: Date,
+    notified: Boolean,
 });
 
 const Task = mongoose.model("Task", taskSchema);
