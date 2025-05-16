fee3dcc07560032225fa73343cc39243f0fccc29
Pearly Li <pli103@my.bcit.ca>
2025-05-16 11:13:11 -0700

    DecoupletaskNotification from server.js

---
+++ backend/taskNotification.js
@@ -0,0 +1,15 @@
+/* Task route for notifications */
+const Task = require("./models/Task");
+
+async function findTasksToNotify() {
+    try {
+        const tasks = await Task.find({
+            notifyAt: { $lte: newDate() },
+            notified: false,
+        });
+        return tasks;
+    } catch (error) {
+        console.error("Error fetching tasks to notify:", error);
+        return []; // adding a fallback return value in case an error occurs so we don't get undefined
+    }
+}
