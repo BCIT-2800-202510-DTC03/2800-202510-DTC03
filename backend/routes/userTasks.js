41e17b70c9b43217cafca2f56ac0a2de7cc41540
Pearly Li <pli103@my.bcit.ca>
2025-05-21 11:17:39 -0700

    Create backend route for userTasks

---
+++ backend/routes/userTasks.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const UserTasks = require("../models/UserTasks");
+
+router.get("userTasks", async (req, res) => {
+    try {
+        const userId = req.session.userId;
+
+        if (!userId) {
+            return res
+                .status(401)
+                .json({ error: "User is not authenticated." });
+        }
+
+        const tasksList = await UserTasks.find({ userId });
+        res.status(200).json(tasksList);
+    } catch (error) {
+        console.error("Failed to fetch user's task list:", error);
+        res.status(500).json({
+            error: "Server error while fetching user's task list.",
+        });
+    }
+});
