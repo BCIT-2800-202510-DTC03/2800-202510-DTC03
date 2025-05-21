d2bc30ec14cf4d9420e233f7fd9130c6706858ae
Pearly Li <pli103@my.bcit.ca>
2025-05-20 18:13:04 -0700

    Add config.js for switching URLs easily depending on deployment

---
+++ frontend/scripts/config.js
@@ -0,0 +1,9 @@
+// This checks if it is local host by whats in the URL
+const isLocalHost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.01";
+
+// This decides what the URL will be depending if its deployed on render or is localhost
+export const backendURL = isLocalHost
+    ? "http://localhost:3000"
+    : "https://two800bloomgreener.onrender.com";
