f9998923b36298f4ccd3b357afe27c61bc6dc1f6
Zoey Jiang <zoeyj8170@gmail.com>
2025-05-21 23:45:48 -0700

    Update the localhost link in landing page

--- frontend/scripts/landing.js
+++ frontend/scripts/landing.js
@@ -1,6 +1,7 @@
 import { DEPLOYED_FRONTEND_URL } from '../util.js';
-
+import { currentBackEndUrl } from '../util.js'
+import { currentFrontEndUrl } from '../util.js'
 document.getElementById('CTA_button').addEventListener("click", () => {
-    window.location.href = `${DEPLOYED_FRONTEND_URL}/pages/login.html`
+    window.location.href = `${currentFrontEndUrl}/pages/login.html`
     console.log("CTA button clicked");
 })
