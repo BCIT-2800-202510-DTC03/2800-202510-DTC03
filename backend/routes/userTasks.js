09da430dcc83a8160774270c83f8e4bb90ec9714
Pearly Li <pli103@my.bcit.ca>
2025-05-21 18:52:44 -0700

    Implement add route

--- backend/routes/userTasks.js
+++ backend/routes/userTasks.js
@@ -22,4 +22,32 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.post("/add", async (req, res) => {
+    const userId = req.session.userId;
+    const { description, category, isAIGenerated } = req.body;
+
+    if (!userId) {
+        return res.status(401).json({ error: "User is not authenticated" });
+    }
+
+    try {
+        const newTask = new UserTasks({
+            userId,
+            description,
+            category,
+            isAIGenerated: isAIGenerated ? isAIGenerated : false,
+            worth: 5,
+            completed: false,
+        });
+        await newTask.save();
+        res.status(201).json({
+            message: "Successfully added task:",
+            task: newTask,
+        });
+    } catch (error) {
+        console.error("Couldn't save user task.", error);
+        res.status(500).json({ error: "Couldn't add user task." });
+    }
+});
+
 module.exports = router;
