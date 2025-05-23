2472790440eec6627e71c72a798f02f04e101bc6
Genevieve <gglaim@my.bcit.ca>
2025-05-23 14:18:51 -0700

    Implement report backend

--- backend/routes/error.js
+++ backend/routes/error.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 
+
+const email = process.env.EMAIL_USER;
+const pass = process.env.EMAIL_PASS;
 router.post("/error/sendReport", async (req, res) => {
     try {
         const { issueType, moreInfo } = req.body;
@@ -10,10 +13,20 @@ router.post("/error/sendReport", async (req, res) => {
         const mailer = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+                user: email,
+                pass: pass
             }
         })
+
+        const mailInfo = {
+            from: `${email}`,
+            to: `${email}`,
+            subject: `Error Report: ${issueType}`,
+            text: `Error Type: ${issueType}\n\nDescription:\n${moreInfo}`
+        }
+
+        await mailer.sendMail(mailInfo);
+        res.status(200).send("Report sucessfully sent.");
     } catch (error) {
         console.error("Error sending report:", error);
     }
