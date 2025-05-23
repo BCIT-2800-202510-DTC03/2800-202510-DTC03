bce92eebf54591cfb9c1d3af10c3a4d765a54f0c
Pearly Li <pli103@my.bcit.ca>
2025-05-23 13:20:34 -0700

    Remove commented out code

--- frontend/util.js
+++ frontend/util.js
@@ -11,8 +11,3 @@ export const backendURL = isLocalHost
 export const frontendURL = isLocalHost
     ? "http://localhost:5500/frontend"
     : "https://two800bloomgreener.onrender.com";
-
-// export const DEPLOYED_FRONTEND_URL = "https://two800bloomgreener.onrender.com";
-// export const LOCAL_FRONTEND_URL = "http://localhost:5500/frontend";
-// export const currentFrontEndUrl = "http://localhost:5500/frontend";
-// export const currentBackEndUrl = "http://localhost:3000";
