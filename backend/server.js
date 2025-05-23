0ca94caf050d92d816693091741dcba1da63cc5a
Zoey Jiang <zoeyj8170@gmail.com>
2025-05-22 18:00:42 -0700

    Update the proxy

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.set("trust proxy", 1);
 app.use(
     session({
         secret: process.env.SESSION_SECRET /* from .env */,
